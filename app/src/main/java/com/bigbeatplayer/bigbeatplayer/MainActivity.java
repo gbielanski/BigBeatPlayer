57b35e6e55e2df857ac7a1276a879cc6280004d0
Grzegorz Bielański <gbielanski@poczta.onet.pl>
2017-06-01 23:51:12 +0200

    Layout for MusicPlayer activity

--- app/src/main/java/com/bigbeatplayer/bigbeatplayer/MainActivity.java
+++ app/src/main/java/com/bigbeatplayer/bigbeatplayer/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                Intent intent = new Intent(MainActivity.this, MusicPlayerActivity.class);
                 startActivity(intent);
             }
         });
